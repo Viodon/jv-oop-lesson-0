f904d71ad3547adc39175be682d368cc1f77d497
Andrii Hryniuk <grinuk0706@gmail.com>
2025-12-17 21:39:07 +0100

    added login and findByEmail methods

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -10,7 +10,14 @@ public class AuthenticationService {
      * @return true if user by email exists and passed password is equal to user's password.
      * Return false in any other cases.
      */
+    private UserService user = new UserService();
+
     public boolean login(String email, String password) {
-        return false;
+        if (user.findByEmail(email) == null) {
+            return false;
+        }
+        return email.equals(user.findByEmail(email).getEmail())
+            && password.equals(user.findByEmail(email).getPassword());
+
     }
 }
